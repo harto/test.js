b116b4b3dd08b5024dfcdb13b83ef0092fb230d1
Stuart Campbell <stuart@harto.org>
2010-07-20 21:58:14 +1000

    restructure project directory

--- bin/test.js
+++ bin/test.js
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-BIN_DIR=$(dirname $0)
-SRC_DIR=$(dirname $BIN_DIR)/src
+ROOT=$(dirname $(dirname $0))
 
-$BIN_DIR/js -f $SRC_DIR/test.js $SRC_DIR/rhino-test.js $@
+BIN_DIR=${ROOT}/bin
+SRC_DIR=${ROOT}/src
+LIB_DIR=${ROOT}/lib
+
+if [[ "$1" == "-b" ]]; then
+    # include browser environment
+    EXTRAS="${LIB_DIR}/env.js"
+fi
+
+${BIN_DIR}/js -f ${SRC_DIR}/test.js ${SRC_DIR}/rhino.js ${EXTRAS} $@
